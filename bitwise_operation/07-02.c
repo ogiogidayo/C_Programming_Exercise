df1f0d177089be77ee006f83497bffbfedfdda2a
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2023-11-22 22:25:04 +0900

    refactor: change algorithm

--- bitwise_operation/07-02.c
+++ bitwise_operation/07-02.c
@@ -1,60 +1,53 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-bool prime_found(int n);
-void store(unsigned char *a);
-void print_bits(unsigned char *a, int size);
+int Is_Prime(int n);
+void store(unsigned char *prime);
 
-int main()
-{
-    unsigned char prime[22] = {0};
+int main() {
+    unsigned char prime[22];
+    memset(prime, 0, sizeof(prime));
     store(prime);
-    print_bits(prime, sizeof(prime));
-    return 0;
-}
 
-bool prime_found(int n)
-{
-    if (n <= 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    }
-    return true;
-}
-void store(unsigned char *a)
-{
-    int count = 0;
-    for (int i = 2; i <= 100; i++)
-    {
-        if (prime_found(i))
-        {
-            int char_index = (count * 7) / 8;
-            int bit_offset = (count * 7) % 8;
-            a[char_index] |= (i & 0x7F) << bit_offset;
-            if (bit_offset + 7 > 8)
-            {
-                a[char_index + 1] |= (i & 0x7F) >> (8 - bit_offset);
-            }
-            count++;
-        }
-    }
-    
-}
-
-void print_bits(unsigned char *a, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 7; j >= 0; j--)
-        {
-            printf("%d", (a[i] >> j) & 1);
+    for (int i = 0; i < 22; i++) {
+        for (int j = 7; j >= 0; j--) {
+            printf("%d", (prime[i] >> j) & 1);
         }
         printf(" ");
     }
+
     printf("\n");
+
+    return 0;
+}
+
+int Is_Prime(int n) {
+    if (n <= 1) 
+        return 0;
+    if (n <= 3) 
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0) 
+        return 0;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return 0;
+    }
+    return 1;
+}
+
+void store(unsigned char *prime) {
+    int bit_index = 0;
+    for (int i = 2; i <= 100; i++) {
+        if (Is_Prime(i)) {
+            for (int j = 7 - 1; j >= 0; j--) {
+                if (bit_index / 8 >= 22) {
+                    break;
+                }
+                int bit = (i >> j) & 1;
+                prime[bit_index / 8] |= bit << (7 - bit_index % 8);
+                bit_index++;
+            }
+        }
+    }
 }
