b789c0f4bb00ef9de29a9d8e9f376557b28c7659
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2023-11-22 21:06:19 +0900

    fix: change algorithm

--- bitwise_operation/07-02.c
+++ bitwise_operation/07-02.c
@@ -1,38 +1,59 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-int isPrimeNumber(unsigned int n) {
-    if (n < 2) {
-        return 0;
-    }
+bool prime_found(int n);
+void store(unsigned char *a);
+void print_bits(unsigned char *a, int size);
 
-    for (unsigned int i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
+int main()
+{
+    unsigned char prime_array[25 * 7 / 8 + 1] = {0};
+    store(prime_array);
+    print_bits(prime_array, sizeof(prime_array));
+    return 0;
 }
 
-int main() {
-    unsigned int primes[30] = {0};
-    for (unsigned int n = 2; n <= 100; n++) {
-        int primeIndex = 0;
-        if (isPrimeNumber(n)) {
-            int arrayIndex = (primeIndex * 7) / 32;
-            int bitPosition = (primeIndex * 7) % 32;
-
-            primes[arrayIndex] |= n << bitPosition;
-
-            primeIndex++;
+bool prime_found(int n)
+{
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+void store(unsigned char *a)
+{
+    int prime_count = 0;
+    for (int i = 2; i <= 100; i++)
+    {
+        if (prime_found(i))
+        {
+            int char_index = (prime_count * 7) / 8;
+            int bit_offset = (prime_count * 7) % 8;
+            a[char_index] |= (i & 0x7F) << bit_offset;
+            if (bit_offset + 7 > 8)
+            {
+                a[char_index + 1] |= (i & 0x7F) >> (8 - bit_offset);
+            }
+            prime_count++;
         }
-    }  
-    for (int i = 0; i < 30; i++) {
-        for (int j = 0; j < 7 && (i * 7 + j) < 25; j++) {
-            int bitPosition = j * 7;
-            unsigned int value = (primes[i] >> bitPosition) & ((1 << 7) - 1);
-            printf("%u ", value);
+    }
+}
+
+void print_bits(unsigned char *a, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 7; j >= 0; j--)
+        {
+            printf("%d", (a[i] >> j) & 1);
         }
+        printf(" ");
     }
     printf("\n");
 }
